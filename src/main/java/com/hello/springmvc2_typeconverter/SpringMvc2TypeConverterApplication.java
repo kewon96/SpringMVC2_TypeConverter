026e93b9ad5d7b3f31d89adbbeefd56fe09c3f19
Kewon <yang0184@naver.com>
2023-04-05 00:18:52 +0900

    Initial commit

---
+++ src/main/java/com/hello/springmvc2_typeconverter/SpringMvc2TypeConverterApplication.java
@@ -0,0 +1,13 @@
+package com.hello.springmvc2_typeconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvc2TypeConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvc2TypeConverterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hello/springmvc2_typeconverter/SpringMvc2TypeConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.springmvc2_typeconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc2TypeConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

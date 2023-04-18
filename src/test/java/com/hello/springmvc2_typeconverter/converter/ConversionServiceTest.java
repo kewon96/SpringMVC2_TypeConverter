68c240dfa230a068858b687c48085a4a8ccb135a
Kewon <yang0184@naver.com>
2023-04-19 01:30:39 +0900

    ConversionServiceTest.java

---
+++ src/test/java/com/hello/springmvc2_typeconverter/converter/ConversionServiceTest.java
@@ -0,0 +1,36 @@
+package com.hello.springmvc2_typeconverter.converter;
+
+import com.hello.springmvc2_typeconverter.type.IpPort;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.DefaultConversionService;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ConversionServiceTest {
+
+    @Test
+    void conversionService() {
+        // 등록
+        DefaultConversionService conversionService = new DefaultConversionService();
+        conversionService.addConverter(new StringToIntegerConverter());
+        conversionService.addConverter(new IntegerToStringConverter());
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+        // 사용
+        Integer result = conversionService.convert("10", Integer.class);
+        System.out.println(result);
+
+        assertThat(conversionService.convert("10", Integer.class)).isEqualTo(10);
+        assertThat(conversionService.convert(10, String.class)).isEqualTo("10");
+
+        IpPort ipPort = new IpPort("127.0.0.1", 8080);
+        String ipPortStr = "127.0.0.1:8080";
+
+        assertThat(conversionService.convert(ipPortStr, IpPort.class)).isEqualTo(ipPort);
+        assertThat(conversionService.convert(ipPort, String.class)).isEqualTo(ipPortStr);
+
+
+    }
+
+}
